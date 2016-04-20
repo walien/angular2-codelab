a05709504fda813fcd2f732480f802a2677fae16
Elian ORIOU <elian.oriou@4sh.fr>
2016-04-20 10:09:08 +0200

    perform step 1

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
 import { bootstrap } from 'angular2/platform/browser'
+import {Ng2CodelabApp} from "./app/ng2-codelab";
 
 // import and bootstrap you angular2 application...
+bootstrap(Ng2CodelabApp, []);
---
+++ src/app/ng2-codelab.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app/ng2codelab.html'
+})
+export class Ng2CodelabApp {
+
+}
